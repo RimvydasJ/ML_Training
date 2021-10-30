c249e87ff004b9217c856fb81e3cc48e1397a4e4
Rimvydas Janušauskas <janus@exis.com.gr>
2021-10-30 21:10:13 +0300

    mean shift

--- kmeans_algorithm.py
+++ kmeans_algorithm.py
@@ -12,6 +12,8 @@ X = np.array([[1, 2],
               [9,11]])
 
 
+colors = 10*["g","r","c","b","k"]
+
 class K_Means:
     #tol is tolerance, if centroids move at this rate or less we say that we are optimized
     def __init__(self, k=2,tol=0.001,max_iter=300):
@@ -42,14 +44,45 @@ class K_Means:
             for classification in self.classifications:
                 self.centroids[classification] = np.average(self.classifications[classification], axis=0)
 
+            optimized = True
+
+            for c in self.centroids:
+                original_centroid = prev_centroids[c]
+                current_centroid = self.centroids[c]
+                if np.sum((current_centroid-original_centroid)/original_centroid*100) > self.tol:
+                    optimized = False
+            if optimized:
+                break
+
 
     def predict(self,data):
-        pass
+        distances = [np.linalg.norm(data-self.centroids[centroid])for centroid in self.centroids]
+        classification = distances.index(min(distances))
+        return classification
 
 
-k_means = K_Means()
+clf = K_Means()
+clf.fit(X)
 
-k_means.fit(X)
+for centroid in clf.centroids:
+    plt.scatter(clf.centroids[centroid][0], clf.centroids[centroid][1], marker="o", color="k", s=150, linewidths=5)
+
+for classification in clf.classifications:
+    color = colors[classification]
+    for featureset in clf.classifications[classification]:
+        plt.scatter(featureset[0], featureset[1], marker="x", color=color, s=150, linewidths=5)
 
 
-plt.scatter(X[:0], X[:1], s=150)
+unknowns = np.array([[1,3],
+                     [8,9],
+                     [0,3],
+                     [5,4],
+                     [6,4],])
+
+
+for unknown in unknowns:
+    classification = clf.predict(unknown)
+    plt.scatter(unknown[0],unknown[1], marker="*", color = colors[classification], s=150, linewidths=5)
+
+
+plt.show()
